0debb66393ea2bb5fb84fd88a4957c954a14e61d
bharathtn <bharathtn12@gmail.com>
2016-08-02 12:56:38 +0530

    hi

--- file.js
+++ file.js
@@ -3,5 +3,5 @@ var fieaddr = process.argv[2];
 
 var buf = fs.readFileSync(fieaddr);
 
-var str = buf.toString().split('\n').length-1;
+var str = buf.toString().split('\n').length - 1;
 console.log(str);
